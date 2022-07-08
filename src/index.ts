1d5f1f13c86285577d861fe185514fe3631bb18b
XDapps <xdev@xdapps.xyz>
2022-07-07 21:39:52 -0400

    5

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './models/image-layer/ImageLayer';
 export * from './models/layer-option/LayerOption';
 export * from './models/project/Project';
+export * from './models/project/PreMintProject';
